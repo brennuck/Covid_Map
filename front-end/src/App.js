93abaf96b6e9dc77de4db05a280ac8b37d4d1f43
Brennon Nuckols <brennuck@gmail.com>
2020-08-20 10:07:07 -0600

    Added processed data

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,36 +2,82 @@ import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
-import Map from './Components/Map.js';
+import Map from "./Components/Map.js";
+import { coordinates } from "./Patch/CountryCordinates.js";
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            countries_data: []
-        }
-    }
-    
+	constructor() {
+		super();
+		this.state = {
+			countries_data: [],
+			fields: ["confirmed", "deaths", "recovered"],
+			query: "confirmed",
+		};
+	}
+
 	componentDidMount() {
 		this.fetchCountryData();
-    }
-    
+	}
+
 	fetchCountryData = async () => {
-        axios.get("https://corona-api.com/countries")
-        .then(res => {
-            this.setState({ countries_data: res.data.data })
-        })
-        .catch(err => {
-            console.log(err);
-        })
-    };
+		axios
+			.get("https://corona-api.com/countries")
+			.then((res) => {
+				this.setState({ countries_data: res.data.data });
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	processData = (data) => {
+		let processed = [];
+
+		for (const d of data) {
+			let obj = {
+				name: d.name,
+				code: d.code,
+				flag: `https://cdn.staticaly.com/gh/hjnilsson/country-flags/master/svg/${d.code.toLowerCase()}.svg`,
+				updated_at: d.updated_at,
+				confirmed: d.latest_data.confirmed,
+				deaths: d.latest_data.deaths,
+				recovered: d.latest_data.recovered,
+			};
+
+			// Patch for countries' coordinates
+			obj["coordinates"] = {
+				latitude:
+					coordinates.find((f) => f.country_code === d.code) !==
+					undefined
+						? coordinates.find((f) => f.country_code === d.code)
+								.latlng[0]
+						: 0,
+				longitude:
+					coordinates.find((f) => f.country_code === d.code) !==
+					undefined
+						? coordinates.find((f) => f.country_code === d.code)
+								.latlng[1]
+						: 0,
+			};
+
+			processed.push(obj);
+		}
+
+		return processed;
+	};
+
+	handleSetQuery = (query) => {
+		this.setState({
+			query,
+		});
+	};
 
 	render() {
-        return (
-            <div className="App">
-                <Map />
-            </div>
-        )
+		return (
+			<div className="App">
+				<Map />
+			</div>
+		);
 	}
 }
 
